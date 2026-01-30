ee1db9132226fa8fdbd6839ac237ebf2bcfbc65e
Tayler Hughes <hello@taylerhughes.co.uk>
2026-01-30 14:20:00 +0000

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to LinkedIn
-    window.location.href = 'https://linkedin.com/in/taylerhughes';
-  }, []);
-
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600 mx-auto mb-4"></div>
-        <p className="text-gray-600">Redirecting to LinkedIn...</p>
-      </div>
-    </div>
-  );
+  redirect('https://linkedin.com/in/taylerhughes');
 }
